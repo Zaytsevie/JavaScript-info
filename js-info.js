66575d76ded016112749bd6588aa748d52c34496
Zaytsevie <115174587+Zaytsevie@users.noreply.github.com>
2022-10-15 15:44:12 +0400

    Add files via upload

---
+++ js-info.js
@@ -0,0 +1,5 @@
+alert("JavaScript разработал - Брендан Эйх")
+alert("JavaScript был разработан за 10 дней")
+alert("JavaScript появился в 1995 году")
+alert("JavaScript - это Java")
+alert("Спецификация языка JavaScript называется ECMAScript")
